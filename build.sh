c5e8fab8335034613fb299ce8bb85b908538105e
winffy2009 <47477623+winffy2009@users.noreply.github.com>
2020-08-23 21:29:53 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-docker build github.com/winffy2009/v2ray-docker
-docker tag v2ray:latest winffy2009/v2ray:latest
+docker build github.com/winffy2009/v2ray-docker -t winffy2009/v2ray:latest
 docker push winffy2009/v2ray:latest
